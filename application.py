3d6c81ce6ebd09c54c1b12739864c9eb476240fd
Thomas Lebrun <lebrun@info22-19.iut45.univ-orleans.fr>
2015-11-12 09:41:20 +0100

    modif application.py, ajout dico

--- application.py
+++ application.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+global dico
+dico = {"michel":["manger","bouger",".com"],
+        "Batman":["say IM THE BATMAN","then wait"],
+        "Pikachu":["pika","pika pika","pika","pika"],
+        "Monsieur oui":["Dire oui","Et encore oui"],
+        None:["L'anonyme est un glandeur"]
+}
+
 @app.route('/')
 def home():
     return render_template(
@@ -12,11 +20,14 @@ def home():
 @app.route('/user/')
 @app.route('/user/<name>')
 def user(name=None):
-    todo=["rien", "pas grand chose", "sieste"]
+    global dico
+    # todo=["rien", "pas grand chose", "sieste"]
+    if name not in dico.keys():
+        dico[name]=['new user without tasks']
     return render_template(
         "user.html",
         name=name,
-        todo=todo)
+        todo=dico[name])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
